0190e23c14f8b06d4d2fc85ca7a3698b20dfb191
usmike226 <michael.nikiema@hotmail.fr>
2024-11-25 13:16:49 -0500

    Create person.java

---
+++ person.java
@@ -0,0 +1,77 @@
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class Person implements Comparable<Person> {
+    private int age;
+    private String name;
+    private String address;
+    private int zipCode;
+    private double salary;
+
+    // Default constructor
+    public Person() {}
+
+    // constructor
+    public Person(int age, String name, String address, int zipCode, double salary) {
+        this.age = age;
+        this.name = name;
+        this.address = address;
+        this.zipCode = zipCode;
+        this.salary = salary;
+    }
+
+    // Accessors and mutators
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    // toString method to format output
+    @Override
+    public String toString() {
+        NumberFormat currencyFormat = NumberFormat.getCurrencyInstance(Locale.US);
+        return String.format("%d %s %s %d %s", 
+                age, name, address, zipCode, currencyFormat.format(salary));
+    }
+
+    // Comparable implementation to sort by salary in descending order
+    @Override
+    public int compareTo(Person other) {
+        return Double.compare(other.salary, this.salary);
+    }
+}
